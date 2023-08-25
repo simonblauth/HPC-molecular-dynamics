53fd974c1cecb64e844d98e8744e5758ba6ab2a7
Simon Blauth <simon.blauth@mailbox.org>
2023-08-25 20:21:48 +0200

    script for simulating and plotting

--- milestones/04/plot_energy.py
+++ milestones/04/plot_energy.py
@@ -3,13 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Plot Total Energy over time.")
-    parser.add_argument("--csv_files", nargs="+", help="Path(s) to the csv file(s) containing the data from the simulation.")
-    parser.add_argument("--labels", nargs="*", help="Labels for the plots made from the csv files.")
-    args = parser.parse_args()
-    filepaths = args.csv_files
-    labels = args.labels if args.labels is not None and len(args.labels) >= len(filepaths) else [f"label_{i}" for i in range(len(filepaths))]
+def plot_total_energy(filepaths: list[str], labels: list[str]):
     for label, filepath in zip(labels, filepaths):
         df = pd.read_csv(filepath)
         x = df["Timestep"]
@@ -19,3 +13,13 @@ if __name__ == "__main__":
     plt.ylabel("Total Energy")
     plt.legend()
     plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Plot Total Energy over time.")
+    parser.add_argument("--csv_files", nargs="+", help="Path(s) to the csv file(s) containing the data from the simulation.")
+    parser.add_argument("--labels", nargs="*", help="Labels for the plots made from the csv files.")
+    args = parser.parse_args()
+    filepaths = args.csv_files
+    labels = args.labels if args.labels is not None and len(args.labels) >= len(filepaths) else [f"label_{i}" for i in range(len(filepaths))]
+    plot_total_energy(filepaths, labels)
