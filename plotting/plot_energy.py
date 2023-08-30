ce38cef9d8343222b69c3fc562be472027702b1f
Simon Blauth <simon.blauth@mailbox.org>
2023-08-30 15:39:47 +0200

    generalize plotting from csv

--- plotting/plot_energy.py
+++ plotting/plot_energy.py
@@ -1,25 +1,55 @@
 import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
+from typing import Optional
 
 
-def plot_total_energy(filepaths: list[str], labels: list[str]):
+def plot_from_csv(filepaths: list[str], labels: list[str], x: str, y: str):
     for label, filepath in zip(labels, filepaths):
         df = pd.read_csv(filepath)
-        x = df["Timestep"]
-        y = df["Total Energy"]
-        plt.plot(x, y, label=label)
-    plt.xlabel("Timestep")
-    plt.ylabel("Total Energy")
+        x_data = df[x]
+        y_data = df[y]
+        plt.plot(x_data, y_data, label=label)
+
+
+def make_plot(filepaths: list[str], labels: list[str], x: str, y: str, xlabel: Optional[str], ylabel: Optional[str]):
+    if xlabel is None:
+        xlabel = x
+    if ylabel is None:
+        ylabel = y
+    plot_from_csv(filepaths, labels, x, y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.legend()
     plt.show()
 
 
+def plot_total_energy(filepaths: list[str], labels: list[str]):
+    make_plot(filepaths, labels,
+              x="Timestep", y="Total Energy",
+              xlabel="Timestep", ylabel="Total Energy"
+              )
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Plot Total Energy over time.")
-    parser.add_argument("--csv_files", nargs="+", help="Path(s) to the csv file(s) containing the data from the simulation.")
-    parser.add_argument("--labels", nargs="*", help="Labels for the plots made from the csv files.")
+    parser = argparse.ArgumentParser(description="Plot from csv files.")
+    parser.add_argument("--csv_files", nargs="+",
+                        help="Path(s) to the csv file(s) containing the data from the simulation.", required=True)
+    parser.add_argument("--labels", nargs="*",
+                        help="Labels for the plots made from the csv files.")
+    parser.add_argument("--xlabel", type=str,
+                        help="Label for the x axis")
+    parser.add_argument("--ylabel", type=str,
+                        help="Label for the y axis")
+    parser.add_argument("--xdata", type=str,
+                        help="Data field for the x axis", required=True)
+    parser.add_argument("--ydata", type=str,
+                        help="Data field for the y axis", required=True)
     args = parser.parse_args()
     filepaths = args.csv_files
-    labels = args.labels if args.labels is not None and len(args.labels) >= len(filepaths) else [f"label_{i}" for i in range(len(filepaths))]
-    plot_total_energy(filepaths, labels)
+    if args.labels is not None and len(args.labels) >= len(filepaths):
+        labels = args.labels
+    else:
+        labels = [f"label_{i}" for i in range(len(filepaths))]
+    make_plot(filepaths, labels, args.xdata,
+              args.ydata, args.xlabel, args.ylabel)
--- plotting/simulate_and_plot.py
+++ plotting/simulate_and_plot.py
@@ -6,6 +6,7 @@ import os.path as osp
 import tempfile
 import time
 from plot_energy import plot_total_energy
+from typing import Optional
 
 
 def total_energy(simulator: str):
@@ -22,7 +23,19 @@ def total_energy(simulator: str):
     plot_total_energy(filepaths, labels)
 
 
-def simulation_time(simulator: str):
+def simulation_times(simulators: list[str], labels: Optional[list[str]] = None):
+    if labels is None:
+        labels = [f"simulator {i}" for i in range(len(simulators))]
+    for label, simulator in zip(labels, simulators):
+        simulation_time(simulator, label)
+    # plt.semilogy()
+    plt.legend()
+    plt.xlabel("Number of Atoms")
+    plt.ylabel("Simulation Time per Timestep in seconds")
+    plt.show()
+
+
+def simulation_time(simulator: str, label):
     lattice_sizes = list(range(2, 11))
     nb_atoms = [math.pow(ls, 3) for ls in lattice_sizes]
     simulation_times = []
@@ -33,20 +46,18 @@ def simulation_time(simulator: str):
         os.system(f"{simulator} --max_timesteps {timesteps} --silent --lattice_size {ls} --cutoff {cutoff}")
         stop = time.time()
         simulation_times.append((stop - start) / timesteps)
-    plt.plot(nb_atoms, simulation_times)
-    # plt.semilogy()
-    plt.xlabel("Number of Atoms")
-    plt.ylabel("Simulation Time per Timestep in seconds")
-    plt.show()
+    plt.plot(nb_atoms, simulation_times, label=label)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simulate and Plot.")
     parser.add_argument("--mode", type=str, choices=["total_energy", "simulation_time"], required=True)
-    parser.add_argument("--simulator", type=str, required=True)
+    parser.add_argument("--simulators", nargs='+', type=str, required=True)
+    parser.add_argument("--labels", nargs='+', type=str)
     args = parser.parse_args()
+    print(args.simulators)
     match args.mode:
         case "total_energy":
-            total_energy(args.simulator)
+            total_energy(args.simulators[0])
         case "simulation_time":
-            simulation_time(args.simulator)
+            simulation_times(args.simulators, args.labels)
