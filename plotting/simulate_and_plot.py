08bfb3aa3e37cc1d37df95e397af5e5b1706b909
Simon Blauth <simon.blauth@mailbox.org>
2023-08-31 11:33:42 +0200

    plot milestone 08

--- plotting/simulate_and_plot.py
+++ plotting/simulate_and_plot.py
@@ -26,6 +26,28 @@ def total_energy(simulator: str):
               )
 
 
+def total_energy_domains(simulator: str):
+    domain_decompositions = [(1, 1, 1), (1, 1, 2), (1, 2, 2), (2, 2, 2)]
+    ndomains = [math.prod(dd) for dd in domain_decompositions]
+    labels = [f"processes={nd}, decomposition={dd}" for nd, dd in zip(ndomains, domain_decompositions)]
+    filepaths = []
+    for nd, dd in zip(ndomains, domain_decompositions):
+        tmp = tempfile.gettempdir()
+        filename = f"domains-{nd}.csv"
+        outpath = osp.join(tmp, "hpc_sims", filename)
+        filepaths.append(outpath)
+        os.makedirs(osp.dirname(outpath), exist_ok=True)
+        command = f"mpirun -n {nd} --oversubscribe {simulator} -i ./milestones/07/cluster_923.xyz --mass 197 --timestep 1 --max_timesteps 10000 --output_interval 100 --cutoff 10.0 --domains {dd[0]} {dd[1]} {dd[2]} --shift_atoms 0.1 --deposit_energy 0.0 --csv {outpath} --silent"
+        scriptpath = osp.join(tmp, "hpc_sims", "doit.sh")
+        with open(scriptpath, "w") as f:
+            f.write(command)
+        os.system(f"sh {scriptpath}")
+    make_plot(filepaths, labels,
+              x="Timestep", y="Total Energy",
+              xlabel="Timestep", ylabel="Total Energy [eV]"
+              )
+
+
 def simulation_times(simulators: list[str], labels: Optional[list[str]] = None):
     if labels is None:
         labels = [f"simulator {i}" for i in range(len(simulators))]
@@ -34,7 +56,7 @@ def simulation_times(simulators: list[str], labels: Optional[list[str]] = None):
     # plt.semilogy()
     plt.legend()
     plt.xlabel("Number of Atoms")
-    plt.ylabel("Simulation Time per Timestep in seconds")
+    plt.ylabel("Simulation Time per Timestep [s]")
     plt.show()
 
 
@@ -63,12 +85,12 @@ def temperature_over_energy(simulator: str, input_files: list[str]):
         os.makedirs(osp.dirname(outpath), exist_ok=True)
         # TODO: fix deposit energies
         os.system(f"{simulator} -i {infile} --cutoff 7 --max_timesteps 50000 --output_interval 100 --timestep 1 --mass 197 --relaxation_time 100 --deposit_energy 0.5 --relaxation_time_deposit 20 --csv {outpath} --initial_relaxation 15000 --temperature 100 --smoothing 0.008 --relaxation_time_increase 10.0")
-    make_plot(filepaths, labels, "Total Energy", "Temperature", ylabel="Temperature in Kelvin", zero_x=True)
+    make_plot(filepaths, labels, "Total Energy", "Temperature", ylabel="Temperature [K]", xlabel="Total Energy [eV]", zero_x=True)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simulate and Plot.")
-    parser.add_argument("--mode", type=str, choices=["total_energy", "simulation_time", "temperature_over_energy"], required=True)
+    parser.add_argument("--mode", type=str, choices=["total_energy", "total_energy_domains", "simulation_time", "temperature_over_energy"], required=True)
     parser.add_argument("--simulators", nargs='+', type=str, required=True)
     parser.add_argument("--labels", nargs='+', type=str)
     args = parser.parse_args()
@@ -78,6 +100,8 @@ if __name__ == "__main__":
             total_energy(args.simulators[0])
         case "simulation_time":
             simulation_times(args.simulators, args.labels)
+        case "total_energy_domains":
+            total_energy_domains(args.simulators[0])
         case "temperature_over_energy":
             # TODO: input files arg
             temperature_over_energy(args.simulators[0], args.input_files)
